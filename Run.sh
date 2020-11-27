8572ecafe5d510f0cd307d51de586c1f09f27215
azumag <tsubasa.azumagakito@gmail.com>
2020-11-28 06:39:41 +0900

    update

--- Run.sh
+++ Run.sh
@@ -6,17 +6,18 @@ xattr -dr com.apple.quarantine ./bin
 # Project settings
 #BASENAME=sample1
 BASENAME=reuben-a
-NumThreads=3
+NumThreads=6
 
 # musicXML_to_label
 SUFFIX=musicxml
 
 # NEUTRINO
-ModelDir=KIRITAN
+ModelDirs=(KIRITAN ITAKO)
+#ModelDir=KIRITAN
 
 # WORLD
-PitchShift=1.0
-FormantShift=1.0
+PitchShifts=(1.0 1.00)
+FormantShifts=(1.00 1.00)
 
 # PATH to current library
 export DYLD_LIBRARY_PATH=$PWD/bin:$DYLD_LIBRARY_PATH
@@ -25,9 +26,27 @@ echo "`date +"%M:%S"` : start MusicXMLtoLabel"
 ./bin/musicXMLtoLabel "score/musicxml/${BASENAME}.${SUFFIX}" "score/label/full/${BASENAME}.lab" "score/label/mono/${BASENAME}.lab"
 
 echo "`date +"%M:%S"` : start NEUTRINO"
-./bin/NEUTRINO "score/label/full/${BASENAME}.lab" "score/label/timing/${BASENAME}.lab" "./output/${BASENAME}.f0" "./output/${BASENAME}.mgc" "./output/${BASENAME}.bap" "./model/${ModelDir}/" -n ${NumThreads} -t
+for i in "${!ModelDirs[@]}"
+do
+
+ModelDir="${ModelDirs[i]}"
+FormantShift="${FormantShifts[i]}"
+PitchShift="${PitchShifts[i]}"
+echo "${ModelDir}"
+echo "${FormantShift}"
+
+echo "score/label/full/${BASENAME}.lab"
+echo "score/label/timing/${ModelDir}-${BASENAME}.lab" 
+echo "./output/${ModelDir}-${BASENAME}.f0"
+echo "./output/${ModelDir}-${BASENAME}.mgc"
+echo "./output/${ModelDir}-${BASENAME}.bap"
+echo "./model/${ModelDir}/"
+
+./bin/NEUTRINO "score/label/full/${BASENAME}.lab" "score/label/timing/${modelDir}-${BASENAME}.lab" "./output/${ModelDir}-${BASENAME}.f0" "./output/${ModelDir}-${BASENAME}.mgc" "./output/${ModelDir}-${BASENAME}.bap" "./model/${ModelDir}/" -n ${NumThreads} -t
 
 echo "`date +"%M:%S"` : start WORLD"
-./bin/WORLD "output/${BASENAME}.f0" "output/${BASENAME}.mgc" "output/${BASENAME}.bap" -f ${PitchShift} -m ${FormantShift} -o "output/${BASENAME}_syn.wav" -n ${NumThreads} -t
+./bin/WORLD "output/${ModelDir}-${BASENAME}.f0" "output/${ModelDir}-${BASENAME}.mgc" "output/${ModelDir}-${BASENAME}.bap" -f ${PitchShift} -m ${FormantShift} -o "output/${ModelDir}-${BASENAME}_syn.wav" -n ${NumThreads} -t
+
+done
 
 echo "`date +"%M:%S"` : END"
